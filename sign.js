b245b367898d84ab1e740666b08541d3bbc7307b
leenaNs <161982545+leenaaa23@users.noreply.github.com>
2025-04-30 17:39:46 +0000

    Add files via upload

---
+++ sign.js
@@ -0,0 +1,16 @@
+// ننتظر تحميل الصفحة بالكامل
+window.addEventListener('DOMContentLoaded', (event) => {
+    let wrapper = document.querySelector('.wrapper'),
+        signUpLink = document.querySelector('.link .signup-link'),
+        signInLink = document.querySelector('.link .signin-link');
+
+    signUpLink.addEventListener('click', () => {
+        wrapper.classList.add('animated-signin');
+        wrapper.classList.remove('animated-signup');
+    });
+
+    signInLink.addEventListener('click', () => {
+        wrapper.classList.add('animated-signup');
+        wrapper.classList.remove('animated-signin');
+    });
+});
